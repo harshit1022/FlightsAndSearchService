55bc97b7e3f2c4f6bf0411d0a4f37e6440493c11
Harshit Patel <harshitpatel1022@gmail.com>
2023-08-24 20:11:40 +0530

    Completed city-repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,7 +3,9 @@ const City = require('../models/index');
 class CityRepository {
   async createCity({ name }) {
     try {
-      const city = await City.create({ name });
+      const city = await City.create({ 
+        name 
+      });
       return city;
     } 
     catch (error) {
@@ -18,9 +20,34 @@ class CityRepository {
           id: cityId
         }
       });
+      return true;
     } 
     catch (error) {
       throw {error};
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      console.log("Encountered some error in city-repository");
+      throw {error};
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where : {
+          id : cityId
+        }
+      });
+      return city;
+    } catch (error) {
+      console.log("Encountered some error in city-repository");
+      throw {error};      
     }
   }
 }
