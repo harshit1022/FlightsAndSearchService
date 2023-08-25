04727f1f5b2d3c692c9fd0c12d25f8e8fdbde0aa
Harshit Patel <harshitpatel1022@gmail.com>
2023-08-25 19:51:38 +0530

    Completed Service Layer

--- src/services/city-service.js
+++ src/services/city-service.js
@@ -1,3 +1,55 @@
 const { CityRepository } = require('../repository/index')
 // here you are importing from the index.js file of repository
 // and here the name HAS TO BE SAME, AS USED THERE WHILE EXPORTING
+
+class CityService {
+  constructor() {
+    this.cityRepository = new CityRepository();
+  }
+
+  async createCity(data) {
+    try {
+      const city = await this.cityRepository.createCity(data);
+      return city;
+    } 
+    catch (error) {
+      console.log("Error occurred in Service Layer");
+      throw {error};
+    }
+  }
+
+  async deleteCity(cityId) {
+    try {
+      const response = await this.cityRepository.deleteCity(cityId);
+      return response;
+    } 
+    catch (error) {
+      console.log("Error occurred in Service Layer");
+      throw {error};
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await this.cityRepository.getCity(cityId);
+      return city;
+    } 
+    catch (error) {
+      console.log("Error occurred in Service Layer");
+      throw {error};
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await this.cityRepository.updateCity(cityId, data);
+      return city;
+    } 
+    catch (error) {
+      console.log("Error occurred in Service Layer");
+      throw {error};
+    }
+  }
+}
+
+module.exports = CityService;
