2b70d76efce2811f4003383205d57b750c870081
Harshit Patel <harshitpatel1022@gmail.com>
2023-09-29 11:52:21 +0530

    Using Sequelize to Retrieve Airport

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyparser = require("body-parser");
 const { PORT } = require('./config/serverConfig'); 
 const ApiRoutes = require('./routes/index');
 
+const {Airport, City} = require('./models/index');
+
 const setupAndStartServer = async () => {
 
   const app = express();
@@ -13,8 +15,50 @@ const setupAndStartServer = async () => {
 
   app.use('/api', ApiRoutes);
 
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`Server started on PORT ${PORT}`);
+
+    // const airports = await Airport.findAll();
+    // const airports = await City.findAll({
+    //   where: {
+    //     id: 2
+    //   },
+    //   include: [
+    //     {
+    //       model: Airport
+    //     }
+    //   ]
+    // });
+    // console.log(airports);
+
+    // const cityName = "Bengalore"; // Replace with the desired city name
+    // City.findOne({
+    //   where: { name: cityName },
+    //   include: Airport, // Include the associated Airport model
+    // })
+    // .then((city) => {
+    //   if (city) {
+    //     // Access the airports associated with the city
+    //     const airports = city.Airports;
+    //     console.log(`Airports in ${cityName}:`);
+    //     airports.forEach((airport) => {
+    //       console.log(airport.name);
+    //     });
+    //   } else {
+    //     console.log(`City ${cityName} not found.`);
+    //   }
+    // })
+    // .catch((error) => {
+    //   console.error('Error:', error);
+    // });
+
+    // const city = await City.findOne({
+    //   where: {
+    //     id: 2
+    //   }
+    // });
+    // const airport = await city.getAirports();
+    // console.log(city, airport);
   })
 }
 
