273a624cef1eed305acbb912fd3632f8e1f575bf
Harshit Patel <harshitpatel1022@gmail.com>
2023-10-07 22:55:47 +0530

    Added CRUD Repository

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,7 +1,9 @@
 module.exports = {
   CityRepository : require('./city-repository'),
   FlightRepository : require('./flight-repository'),
-  AirplaneRepository : require('./airplane-repository')
+  AirplaneRepository : require('./airplane-repository'),
+  AirportRepository : require('./airport-repository'),
+  CrudRepository : require('./crud-repository')
 }
 // you are importing from city-repository.js, airport-repository.js, flight-repository.js, 
 // and then exporting, so that you have to just require 1 time in the main file...writing here require 3 times saves you from writing in the main file
